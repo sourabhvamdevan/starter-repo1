be111b2875e039b477b8e815649a128b870d7a0d
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-03-13 11:57:11 +0530

    Add files via upload

---
+++ task2/data_munger.py
@@ -0,0 +1,40 @@
+import csv
+import os
+
+DATA_DIRECTORY = "./data"
+OUTPUT_FILE_PATH = "./formatted_data.csv"
+
+with open(OUTPUT_FILE_PATH, "w") as output_file:
+    writer = csv.writer(output_file)
+
+    
+    header = ["sales", "date", "region"]
+    writer.writerow(header)
+
+    
+    for file_name in os.listdir(DATA_DIRECTORY):
+        
+        with open(f"{DATA_DIRECTORY}/{file_name}", "r") as input_file:
+            reader = csv.reader(input_file)
+            
+            row_index = 0
+            for input_row in reader:
+                
+                if row_index > 0:
+                    
+                    product = input_row[0]
+                    raw_price = input_row[1]
+                    quantity = input_row[2]
+                    transaction_date = input_row[3]
+                    region = input_row[4]
+
+                    
+                    if product == "pink morsel":
+                        
+                        price = float(raw_price[1:])
+                        sale = price * int(quantity)
+
+                        
+                        output_row = [sale, transaction_date, region]
+                        writer.writerow(output_row)
+                row_index += 1
